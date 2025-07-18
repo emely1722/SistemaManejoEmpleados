7b9609d0c86dfdb19e8aa2e7a8c6b705982bf40e
emely1722 <66326292+emely1722@users.noreply.github.com>
2025-07-17 20:34:50 -0400

    Creación de Controllers

---
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/Controllers/DepartamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaManejoEmpleados.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaManejoEmpleados/SistemaManejoEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaManejoEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
